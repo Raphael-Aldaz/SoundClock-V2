bdd88daf83a289003278b4ee01087a5c51778836
Raphael-Aldaz <raphael.aldaz@gmail.com>
2022-05-12 17:34:32 +0200

    deleting react audio player and using instead react-h5-audio-player, modifying some style for the card and responsive

--- src/Component/MainGrid/Card.js
+++ src/Component/MainGrid/Card.js
@@ -1,10 +1,12 @@
-import ReactPlayer from 'react-player';
-import './MainGrid.scss';
+/*import ReactPlayer from 'react-player';*/
+
 import sound from './honey-im-home-giulio-fazio-main-version-02-37-3206.mp3';
 import image from './téléchargement.jpeg'
 import { useDispatch, useSelector } from 'react-redux';
 import { playingMusics } from '../../actions';
-
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
+import './MainGrid.scss';
 const Card = () => {
     const dispatch = useDispatch()
     const play = useSelector((state)=> state.musics.play)
@@ -17,27 +19,31 @@ const Card = () => {
     const handlePlay = () => {
        dispatch(playingMusics());
     }
-    const handlePause = () => {
-        dispatch(playingMusics());
-    }
+   
     
     return(
-        <div className='card_container'>
+       
             <div className='card'>
-            <ReactPlayer
-            className='player'
-                style={{
-        background: `url(${image}) no-repeat`, 
-    }
-                }
-                url={sound}
-                playing={play}
-            />
+            <AudioPlayer
+                src={sound}
+                className='player'
+             />
 
-                <button onClick={handlePlay}>Play</button>
-                <button onClick={handlePause}>Pause</button>
+{/*                 <ReactPlayer
+                    className='player'
+                    style={
+                        {
+                            background: `url(${image}) no-repeat`, 
+                        }
+                    }
+                    url={sound}
+                    playing={play}
+                    controls
+                    
+                />  */}
+                {/* <button className='button_control' onClick={handlePlay}>Play</button> */}
             </div>
-        </div>
+   
        
 
     );
--- src/Component/MainGrid/index.js
+++ src/Component/MainGrid/index.js
@@ -4,8 +4,27 @@ import './MainGrid.scss';
 const MainGrid = () => {
     return(
         <section className="mainGrid">
+        
+        <h1>Top 10 musics</h1>
+        
         <div className='mainGrid_topMusic'>
             <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            
+        </div>
+            
+
+        
+        <div className='mainGrid_artiste'>
+           
 
         </div>
         </section>
